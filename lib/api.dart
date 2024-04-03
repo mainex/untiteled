eee2be3d7419a2ed8353e70f18fca7c63c502ea5
Kseniia Marchenko <ksumar4enko@gmail.com>
2024-04-03 11:12:53 +0300

    comments for API functions

--- lib/api.dart
+++ lib/api.dart
@@ -2,12 +2,14 @@ import 'package:http/http.dart' as http;
 import 'film.dart';
 import 'dart:convert';
 
+// headers for API authorization
 Map<String, String> requestHeaders = {
   'accept': 'application/json',
   'Authorization':
       'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzU2NDcxNTJiYzE2ZDJjMTk5N2RjOWM3YjZjZDhhMCIsInN1YiI6IjY1ZTg5ZjgzYzE1Zjg5MDE4NjE3OTZiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Y1KsiSdRMVmY2_B4iW_ofV14-HMsfNz5iNBcVHqIVqY'
 };
 
+// Get a list of movies that are currently in theatres (API response)
 Future<String> _getNowInCinema() async {
   Uri uri = Uri.parse(
       'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1');
@@ -15,6 +17,7 @@ Future<String> _getNowInCinema() async {
   return response.body.toString();
 }
 
+// Get a list of movies that are currently in theatres (map of films)
 Future<Map<int, Film>> getNowInCinemaList() async {
   String data = await _getNowInCinema();
   final jsonData = json.decode(data)['results'];
@@ -26,6 +29,7 @@ Future<Map<int, Film>> getNowInCinemaList() async {
   return map;
 }
 
+// Get a list of movies that are being released soon (API response)
 Future<String> _getUpcoming() async {
   Uri uri = Uri.parse(
       'https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1');
@@ -33,6 +37,7 @@ Future<String> _getUpcoming() async {
   return response.body.toString();
 }
 
+// Get a list of movies that are being released soon (map of films)
 Future<Map<int, Film>> getReleaseCalendarList() async {
   String data = await _getUpcoming();
   final jsonData = json.decode(data)['results'];
@@ -44,6 +49,7 @@ Future<Map<int, Film>> getReleaseCalendarList() async {
   return map;
 }
 
+// Search for movies by their original, translated and alternative titles (API response)
 Future<String> _searchMovie(String query) async {
   Uri uri = Uri.parse('https://api.themoviedb.org/3/search/movie?query=' +
       query +
@@ -52,6 +58,7 @@ Future<String> _searchMovie(String query) async {
   return response.body.toString();
 }
 
+// Search for movies by their original, translated and alternative titles (list of Films)
 Future<List<Film>> searchMovie(String query) async {
   String data = await _searchMovie(query);
   final jsonData = json.decode(data)['results'];
